143f8a40033e8c55c161848b0fcf9f8a0556946c
BeetSoup128 <3288982078@qq.com>
2023-06-30 13:24:02 +0800

    first rev

---
+++ GUI.py
@@ -0,0 +1,384 @@
+from masiro import Masiro, MSOEpub
+import tkinter
+import os
+from tkinter import ttk
+import threading
+import json
+import configparser
+import logging
+
+
+import sv_ttk
+
+sv_ttk.set_theme("dark")
+
+
+class MSP(ttk.Frame):
+    if os.path.exists("GUI.ini"):
+        conf = configparser.ConfigParser()
+        conf.read("GUI.ini")
+        try:
+            Local_nam = tkinter.Variable(value=conf['user']['name'])
+            Local_pwd = tkinter.Variable(value=conf['user']['pwd'])
+        except BaseException:
+            Local_nam = tkinter.Variable(value="输入用户名")
+            Local_pwd = tkinter.Variable(value="输入密码")
+    Local_url = tkinter.Variable(value="https://masiro.me")
+    Local_State = tkinter.Variable(value="未登录")
+
+    Local_Login = False
+
+    def __init__(self, master, MSO: Masiro) -> None:
+        super().__init__(master)
+        self.Local_MSO = MSO
+        self.grid(column=0, row=0)
+        ttk.Label(
+            self,
+            text="Masiro_login_page",
+            font=(
+                "-size",
+                48)).grid(
+            column=0,
+            columnspan=2,
+            row=0)
+
+        ttk.Label(
+            self,
+            text="默认url",
+            font=(
+                "-size",
+                24),
+            width=8).grid(
+            column=0,
+            row=1,
+            sticky='w')
+        ttk.Label(
+            self,
+            text="用户名",
+            font=(
+                "-size",
+                36),
+            width=8).grid(
+            column=0,
+            row=2,
+            sticky='w')
+        ttk.Label(
+            self,
+            text="密码  ",
+            font=(
+                "-size",
+                36),
+            width=8).grid(
+            column=0,
+            row=3,
+            sticky='w')
+
+        ttk.Entry(
+            self,
+            textvariable=self.Local_url,
+            font=(
+                "-size",
+                24)).grid(
+            column=1,
+            row=1,
+            sticky='we')
+        ttk.Entry(
+            self,
+            textvariable=self.Local_nam,
+            font=(
+                "-size",
+                36)).grid(
+            column=1,
+            row=2,
+            sticky='w')
+        ttk.Entry(
+            self,
+            textvariable=self.Local_pwd,
+            font=(
+                "-size",
+                36)).grid(
+            column=1,
+            row=3,
+            sticky='w')
+
+        ttk.Label(
+            self,
+            font=(
+                "-size",
+                36),
+            textvariable=self.Local_State,
+            width=8).grid(
+            column=0,
+            columnspan=2,
+            row=4,
+            sticky='w')
+        ttk.Button(
+            self,
+            text="LOGIN",
+            command=self.login,
+            width=12,
+            style='my.TButton').grid(
+            column=0,
+            columnspan=2,
+            row=5)
+
+    def login(self):
+        if not self.Local_Login:
+            threading.Thread(target=self.login_main).start()
+
+    def login_main(self):
+        # print(f"name    ::{ self.Local_nam.get() }\npassword::{ self.Local_pwd.get() }")
+        self.Local_Login = True
+        self.Local_State.set("登陆中")
+        try:
+            if self.Local_MSO.login(self.Local_nam.get(
+            ), self.Local_pwd.get(), self.Local_url.get()):
+                self.Local_State.set("已登录")
+            else:
+                self.Local_State.set("登录异常")
+        except BaseException:
+            self.Local_State.set("网络异常")
+        self.Local_Login = False
+
+
+class NVP(ttk.Frame):
+    Local_NID = tkinter.Variable(value="输入Nid")
+    Local_BTNV = tkinter.Variable(value="Download it")
+    Local_searching = False
+    Local_searched = False
+    Local_Downloading = False
+    Local_Downloaded = False
+
+    def __init__(self, master, MSO: Masiro) -> None:
+        self.MSO = MSO
+        super().__init__(master)
+        self.grid(column=0, row=0)
+        ttk.Entry(
+            self,
+            textvariable=self.Local_NID,
+            font=(
+                "-size",
+                36),
+            width='16').grid(
+            column=0,
+            columnspan=2,
+            row=0,
+            sticky='we')
+        ttk.Button(
+            self,
+            text="搜索",
+            command=self.search,
+            style='my.TButton').grid(
+            column=2,
+            row=0,
+            sticky='we')
+
+        self.t = ttk.Treeview(
+            self,
+            columns=('ep'),
+            show='tree headings',
+            displaycolumns='#all',
+            selectmode='extended')
+        self.t.grid(column=0, row=1, columnspan=3, sticky='we')
+
+        ttk.Button(
+            self,
+            text=" Delete ",
+            command=self.delbktree,
+            style='my.TButton').grid(
+            column=0,
+            row=2)
+        ttk.Button(
+            self,
+            text=" Clear ",
+            command=self.search_clear,
+            style='my.TButton').grid(
+            column=1,
+            row=2)
+        ttk.Button(
+            self,
+            textvariable=self.Local_BTNV,
+            command=self.download,
+            style='my.TButton').grid(
+            column=2,
+            row=2)
+
+    def search(self):
+        if self.Local_searched:
+            self.Local_NID.set("Searched")
+            return
+        if self.Local_searching:
+            self.Local_NID.set("Searching")
+            return
+        threading.Thread(target=self.search_main).start()
+
+    def search_main(self):
+        self.Local_searching = True
+
+        gen = self.MSO.NovSearch(self.Local_NID.get())
+        Title = next(gen)
+        meta = next(gen)
+        bktree = next(gen)
+        if (not Title) or (not meta) or (not bktree):
+            self.Local_BTNV.set("网络异常!")
+            self.Local_searching = False
+            return None
+        ####
+        self.t.heading('#0', text="章节名", anchor='w')
+        self.t.heading('ep', text="EPid", anchor='w')
+        for i in bktree.keys():
+            node = self.t.insert('', 'end', text=i)
+            for t in bktree[i]:
+                self.t.insert(
+                    node, 'end', text=t['tTitle'], values=(
+                        t['path'].split(
+                            "?", 1)[1],))
+        self.Local_searching = False
+        self.Local_searched = True
+
+    def delbktree(self):
+        self.t.delete(*self.t.selection())
+
+    def search_clear(self):
+        self.t.delete(*self.t.get_children())
+        self.Local_searched = False
+        self.Local_Downloaded = False
+        self.MSO.bookMeta = None
+        self.MSO.bookTit = None
+        self.MSO.booktree = None
+        self.MSO.nid = None
+
+    def download(self):
+        if self.Local_Downloaded:
+            self.Local_BTNV.set("Downloaded")
+            return
+        if self.Local_Downloading:
+            self.Local_BTNV.set("Downloading")
+            return
+        threading.Thread(target=self.download_main).start()
+
+    def download_main(self):
+        self.Local_Downloading = True
+        bkt = {}
+        for ch in self.t.get_children():
+            chnode = []
+            for ep in self.t.get_children(ch):
+                chnode.append({'cid=': str(self.t.item(ep)['values'][0])})
+            bkt.update({str(self.t.item(ch)['text']): chnode})
+        self.MSO.booktree = bkt
+        if not os.path.exists(f"Cache/{self.MSO.nid}"):
+            os.mkdir(f"Cache/{self.MSO.nid}")
+        with open(f"Cache/{self.MSO.nid}/init.json", 'w', encoding='utf-8') as f:
+            json.dump(bkt, f)
+        with open(f"Cache/{self.MSO.nid}/meta.json", 'w', encoding='utf-8') as f:
+            json.dump(self.MSO.bookMeta, f)
+        tls = []
+        for _ in bkt.keys():
+            for ep in bkt[_]:
+                cid = ep["cid="]
+                epb, tit = self.MSO.EPSearch(cid=cid)
+                ep["Title"] = tit
+                cid = cid.replace("=", '_')
+                tls.append(
+                    threading.Thread(
+                        target=self.MSO.EPdr,
+                        kwargs={
+                            'ep': epb,
+                            'SavePath': f"Cache/{self.MSO.nid}/{ cid }.xhtml"}))
+                tls[-1].start()
+        [_.join() for _ in tls]
+        with open(f"Cache/{self.MSO.nid}/init.json", 'w', encoding='utf-8') as f:
+            json.dump(bkt, f)
+        self.MSO.modconf(upd={self.MSO.nid: self.MSO.bookTit})
+        self.Local_Downloading = False
+        self.Local_Downloaded = False
+        return
+
+
+class HDP(ttk.Frame):
+    def __init__(self, master, MSO) -> None:
+        super().__init__(master)
+        self.grid(column=0, row=0)
+        self.t = ttk.Treeview(
+            self,
+            columns=(
+                'nid',
+                'title'),
+            selectmode='browse',
+            show='tree')
+        self.t.grid(column=0, columnspan=3, row=0)
+        self.sav = ttk.Button(
+            self,
+            text="Save to ./Result",
+            command=self.save,
+            style='my.TButton')
+        self.sav.grid(column=0, columnspan=2, row=1)
+        self.ref = ttk.Button(
+            self,
+            text="Refresh",
+            command=self.refresh,
+            style='my.TButton')
+        self.ref.grid(column=2, columnspan=1, row=1)
+        self.refresh()
+
+    def save(self):
+        if len(self.t.selection()) == 0:
+            return
+        r = self.t.item(self.t.selection()[0])["values"]
+        threading.Thread(target=MSOEpub(
+            *(self.t.item(self.t.selection()[0]))["values"]).out).start()
+        print(r)
+
+    def refresh(self):
+        with open("./Cache/sav.json", 'r', encoding='utf-8') as f:
+            self.sav = json.load(f)
+        self.t.heading('nid', text="NovelID", anchor='w')
+        self.t.heading('title', text="BookName", anchor='w')
+        del self.sav["nid="]
+        for nids in self.sav.keys():
+            self.t.insert('', 'end', values=(nids, self.sav[nids]))
+
+
+class MSOI(ttk.Notebook):
+    MSO = None
+
+    def _sty_(self, sty: ttk.Style):
+        sty.configure('my.TButton', font=('-size', 36))
+
+    def __init__(self, master, sty) -> None:
+        self.MSO = Masiro()
+        self._sty_(sty)
+        super().__init__(master)
+        self.interpagedict = {
+            "登录": MSP(
+                self, self.MSO), "小说选取": NVP(
+                self, self.MSO), "生成器": HDP(
+                self, self.MSO)}
+        for att in self.interpagedict:
+            self.add(
+                self.interpagedict[att],
+                text=att,
+                padding=12,
+                sticky="nsew")
+        self.grid(column=0, row=0)
+
+
+class app(tkinter.Tk):
+    def __init__(self, SN: str = "MasiroInstaller", BN: str = "MSOI",
+                 CN: str = "Masiro Installer by BeetSoup128"):
+        super().__init__(SN, BN, CN)
+        self.geometry('720x480')
+        self.Local_MSOI = MSOI(self, ttk.Style())
+
+    def run(self, debug=False):
+        self.mainloop()
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.DEBUG,
+                        format='%(asctime)s %(filename)s[line:%(lineno)d] %(levelname)s %(message)s',
+                        datefmt='%a, %d %b %Y %H:%M:%S',
+                        filename='/MSOInstaller.log',
+                        filemode='w')
+
+    app().run(False)
---
+++ masiro.py
@@ -0,0 +1,258 @@
+from selenium import webdriver
+from ebooklib import epub
+import bs4
+import zhconv
+
+
+import time
+import os
+import requests
+import base64
+import logging
+import json
+import uuid
+import re
+
+
+def string_format(iStr: str) -> str:
+    return iStr.replace("&nbsp;", '').replace("\xa0", '').replace(
+        "\u3000", '').replace("\u2800", '').replace('\\n', '').strip()
+
+
+class Masiro:
+    if not os.path.exists("Cache"):
+        os.mkdir("Cache")
+        with open("./Cache/sav.json", 'w', encoding='utf-8') as f:
+            json.dump({"nid=": "BookName"}, f)
+    if not os.path.exists("./Cache/sav.json"):
+        with open("./Cache/sav.json", 'w', encoding='utf-8') as f:
+            json.dump({"nid=": "BookName"}, f)
+    if not os.path.exists("Result"):
+        os.mkdir("Result")
+    with open("./Cache/sav.json", 'r', encoding='utf-8') as f:
+        conf = dict(json.load(f))
+    nid = None
+
+    def __init__(self, debug=False) -> None:
+        ops = webdriver.EdgeOptions()
+        if debug:
+            ops.add_argument("-headless")
+        logging.debug("init browser")
+        self.Web = webdriver.ChromiumEdge(options=ops)
+        logging.debug("init browser done")
+
+    def modconf(self, upd: dict = {}, remove: list = []) -> None:
+        logging.debug(
+            "try modeify conf:{} to update{} and remove{}".format(
+                self.conf, upd, remove))
+        self.conf.update(upd)
+        for rm in remove:
+            self.conf[rm] = None
+            del self.conf[rm]
+        with open("./Cache/sav.json", '2', encoding='utf-8') as f:
+            json.dump(self.conf, f)
+
+    def login(self, name: str, pwd: str, msoHOST: str = "https://masiro.me",
+              loginPATH: str = "/admin/auth/login", loginOKPATH: str = "/admin") -> bool:
+        logging.debug("Login in, using url:" + msoHOST + loginPATH)
+        self.mso = msoHOST
+        self.Web.get(msoHOST + loginPATH)
+        self.Web.execute_script(
+            f"document.getElementById('username').value = '{name}';document.getElementById('password').value = '{pwd}';")
+        time.sleep(2)
+        self.Web.execute_script(
+            "document.getElementById('login-btn').click();")
+        time.sleep(5)
+        fph = self.Web.execute_script("return new URL(document.URL).pathname;")
+        if fph == loginOKPATH:
+            logging.info(f"login OK")
+            logging.debug(
+                f"name:{name}, password:{pwd}, using host:{msoHOST}, using loginpath and final jump2'{loginPATH}' and '{loginOKPATH}'")
+            return True
+        else:
+            logging.info(f"login error, wrong jump path '{fph}'")
+            logging.debug(
+                f"name:{name}, password:{pwd}, using host:{msoHOST}, using loginpath and final jump2'{loginPATH}' and '{loginOKPATH}'")
+            return False
+
+    def NovSearch(self, nid,
+                  nidpathformat: str = r"/admin/novelView?novel_id={nid}"):
+        logging.debug(
+            "search novel's menu using url:" +
+            self.mso +
+            nidpathformat.format(
+                nid=nid))
+        # Title Search
+        if self.nid is None:
+            self.nid = str(nid)
+        else:
+            logging.info("has nid yet while searching {}".format(nid))
+            yield False
+        try:
+            self.Web.get(self.mso + nidpathformat.format(nid=nid))
+            ####
+            Tit = string_format(self.Web.execute_script(
+                "return document.getElementsByClassName('novel-title')[0].innerText;"))
+        except BaseException:
+            logging.info("Error while searching title")
+            yield False
+        else:
+            self.bookTit = Tit
+            yield Tit  # str Title
+        ####
+        # meta-data Search
+        try:
+            metals = string_format(self.Web.execute_script(
+                "return document.getElementsByClassName('n-detail')[0].innerText;")).split('\n')
+            meta = {'brief': string_format(self.Web.execute_script(
+                "return document.getElementsByClassName('brief')[0].innerText;"))}
+            for l in metals:
+                r = l.split(':', 1)
+                meta[r[0].strip()] = r[1].strip()
+        except BaseException:
+            logging.info("Error while searching meta")
+            yield False
+        else:
+            self.bookMeta = meta
+            yield meta  # {brief:..., ...:... }
+        ####
+        # Chapter:Pages Search
+        try:
+            oHtml = self.Web.execute_script(
+                "return document.getElementsByClassName('chapter-ul')[0].outerHTML;")
+            sp = bs4.BeautifulSoup(oHtml)
+            # with open('cache.html','r',encoding='utf-8') as f:
+            #    sp = bs4.BeautifulSoup(f.read(),"html.parser")
+            CHNames = [bTag.string.strip()
+                       for bTag in sp.select("li[class='chapter-box'] b")]
+            EPLists = [[{"path": als["href"], "tTitle":als.select_one("span").string.replace(
+                "&nbsp;", ' ').strip()} for als in ul.select("a")] for ul in sp.select("ul[class='episode-ul']")]
+            res = {}
+            res.update(zip(CHNames, EPLists))
+        except BaseException:
+            logging.info("Error while searching cid")
+            yield False
+        else:
+            self.booktree = res
+            yield res  # {章节:[{path:,tTitle:}],...}
+        ####
+
+    def EPSearch(
+            self, cid: str, epp: str = "/admin/novelReading?") -> tuple[bs4.BeautifulSoup, str]:
+        'MSOPATH + epp + cid\n"https://masiro.me" + "/admin/novelReading?" + "cid=123456"'
+        logging.debug("search EP using url:{}".format(self.mso + epp + cid))
+        self.Web.get(self.mso + epp + cid)
+        time.sleep(5)
+        try:
+            chTitle = str(self.Web.execute_script(
+                "return document.getElementsByClassName('novel-title')[0].innerText;"))
+            chHTML = str(self.Web.execute_script(
+                "return document.getElementsByClassName('box-body nvl-content')[0].outerHTML;"))
+        except BaseException:
+            logging.info("unable to read, try to pay for reading")
+            self.Web.execute_script(
+                "document.getElementsByClassName('hint-btn pay')[0].click();")
+            time.sleep(1)
+            self.Web.execute_script(
+                "document.getElementsByClassName('layui-layer-btn0')[0].click();")
+            time.sleep(1)
+            self.Web.execute_script(
+                "document.getElementsByClassName('layui-layer-btn0')[0].click();")
+            time.sleep(5)
+
+            chTitle = str(self.Web.execute_script(
+                "return document.getElementsByClassName('novel-title')[0].innerText;"))
+            chHTML = str(self.Web.execute_script(
+                "return document.getElementsByClassName('box-body nvl-content')[0].outerHTML;"))
+        chHTML = string_format(chHTML)
+        chTitle = string_format(chTitle)
+        logging.debug("search OK")
+        return (bs4.BeautifulSoup('\n'.join(['<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"',
+                                             '"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">',
+                                             '<html xmlns="http://www.w3.org/1999/xhtml">',
+                                             '<head>\n  <meta charset="utf-8">\n  <title>' + chTitle + '</title>\n</head>\n<body>\n', chHTML, '</body>\n</html>']),
+                                  "html.parser"),
+                chTitle)
+
+    def EPdr(self, ep: bs4.BeautifulSoup, SavePath: str) -> None:
+        for img in ep.find_all("img"):
+            if img.parent.name == 'div':
+                pass
+            tmp = img.attrs
+            url = tmp.get("src", "")
+            if "base64" not in url:
+                time.sleep(2)
+                try:
+                    res = base64.b64encode(requests.get(url).content)
+                except BaseException:
+                    pass
+                else:
+                    tmp["src"] = "data:image/jpeg;base64," + \
+                        res.decode("utf-8")
+            tmp["style"] = "width: auto; height: auto; max-width: 100%; max-height: 100%;"
+            img.append(ep.new_tag('img', **tmp))
+            img.attrs = {"style": "width:100%;"}
+            img.name = 'div'
+        for p in ep.select('*'):
+            if p.string:
+                p.string = zhconv.convert(
+                    p.string, "zh-hans").replace(
+                    '“', '「').replace(
+                    '”', '」').replace(
+                    '‘', '『').replace(
+                    '’', '』')
+        with open(SavePath, 'w', encoding='utf-8') as f:
+            f.write(ep.prettify())
+
+
+class MSOEpub:
+    def __init__(self, nid: str, tit: str) -> None:
+        self.nid = nid
+        self.tit = tit
+        with open(f"./Cache/{nid}/init.json", 'r', encoding='utf-8') as f:
+            self.bktree = json.load(f)
+        with open(f"./Cache/{nid}/meta.json", 'r', encoding='utf-8') as f:
+            self.bkmeta = json.load(f)
+        bk = epub.EpubBook()
+        bk.set_language('zh')
+        bk.set_title(tit)
+        bk.set_identifier(str(uuid.uuid5(namespace=uuid.NAMESPACE_URL,
+                                         name=f"MSOI://BeetSoup128:BeetSoup128@masiro.me/epub/nid={nid}").int))
+        bk.add_author(self.bkmeta.get('作者', 'set_author_here'))
+        bk.add_metadata(
+            "DC", "description", self.bkmeta.get(
+                'biref', 'input_brief_here'))
+        for k in self.bkmeta.keys():
+            bk.add_metadata("masiro", k, self.bkmeta[k])
+        spine_tmp = []
+        toc_tmp = []
+        for ch in self.bktree.keys():
+            eps = []
+            for ep in self.bktree[ch]:
+                with open("./Cache/{}/{}.xhtml".format(nid, ep["cid="].replace('=', '_')), 'r', encoding='utf-8') as f:
+                    page = epub.EpubHtml(title=ep["Title"],
+                                         file_name=ep["cid="].replace(
+                                             '=', '_') + ".xhtml",
+                                         lang='cn',
+                                         content=re.sub(u"[\\x00-\\x08\\x0b\\x0e-\\x1f\\x7f]", '', f.read()))
+                # print(page.content)
+                bk.add_item(page)
+                spine_tmp.append(page)
+                eps.append(page)
+            toc_tmp.append(
+                (epub.Section(
+                    re.sub(
+                        u"[\\x00-\\x08\\x0b\\x0e-\\x1f\\x7f]",
+                        '',
+                        ch)),
+                    eps,
+                 ))
+        bk.toc = [(epub.Section(tit), toc_tmp,)]
+        bk.add_item(epub.EpubNcx())
+        bk.add_item(epub.EpubNav())
+        bk.spine.append("nav")
+        bk.spine.extend(spine_tmp)
+        self.bk = bk
+
+    def out(self):
+        epub.write_epub(f"./Result/{self.tit}.ePub", self.bk)
